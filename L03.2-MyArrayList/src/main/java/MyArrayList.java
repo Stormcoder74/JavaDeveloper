9f23c0ecb4c4151c482e3dd3e04b5b619629937a
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-06-12 15:22:18 +1100

    пишем свой MyArrayList

---
+++ L03.2-MyArrayList/src/main/java/MyArrayList.java
@@ -0,0 +1,196 @@
+import java.util.*;
+
+
+public class MyArrayList<T> implements List<T> {
+
+    private T[] array;
+    private int size;
+    private int capacity;
+
+    public MyArrayList() {
+        this(15);
+    }
+
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity > 0)
+            capacity = initialCapacity;
+        size = 0;
+        array = (T[]) new Object[capacity];
+
+    }
+
+    static <T> void copy(List<? super T> dest, List<? extends T> src) {
+        int srcSize = src.size();
+        int destSize = dest.size();
+        for (int i = 0; i < srcSize && i < destSize; i++)
+            dest.set(i, src.get(i));
+        if(srcSize > destSize)
+            for(int i = destSize; i < srcSize; i++)
+                dest.add(src.get(i));
+    }
+
+    static <T> void sort(List<T> list, Comparator<? super T> c){
+        if(list != null){
+            T[] a = (T[]) list.toArray();
+            Arrays.sort(a, c);
+            list.clear();
+            Collections.addAll(list, a);
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean add(T element) {
+        add(size, element);
+        return true;
+    }
+
+    public void add(int index, T element) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        if (size == capacity) {
+            capacity *= 2;
+            array = Arrays.copyOf(array, capacity);
+        }
+        System.arraycopy(array, index, array, index + 1, size++ - index);
+        array[index] = element;
+    }
+
+    public boolean addAll(Collection<? extends T> c) {
+        for (T t : c) {
+            add(t);
+        }
+        return true;
+    }
+
+    public boolean addAll(int index, Collection<? extends T> c) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+
+        int cSize = c.size();
+
+        if (size + cSize >= capacity) {
+            capacity += cSize;
+            array = Arrays.copyOf(array, capacity);
+        }
+
+        System.arraycopy(array, index, array, index + cSize, size - index);
+        size += cSize;
+
+        int i = index;
+        for (T t : c) {
+            array[i] = t;
+            i++;
+        }
+        return true;
+    }
+
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public Iterator iterator() {
+        return new MyItr();
+    }
+
+    public Object[] toArray() {
+        return Arrays.copyOf(array, size);
+    }
+
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    public void clear() {
+        size = 0;
+        array = (T[]) new Object[capacity];
+    }
+
+    public T get(int index) {
+        if (index >= 0 && index < size)
+            return array[index];
+        return null;
+    }
+
+    public T set(int index, T element) {
+        T respond = null;
+        if (index >= 0 && index < size){
+            respond = array[index];
+            array[index] = element;
+        }
+        return respond;
+    }
+
+    public T remove(int index) {
+        T response = null;
+        if (index >= 0 && index < size){
+            response = array[index];
+            System.arraycopy(array, index + 1, array, index, size - index);
+            size--;
+        }
+        return response;
+    }
+
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    private class MyItr implements Iterator<T>{
+        int nextElem;
+
+        public MyItr() {
+            nextElem = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextElem != size;
+        }
+
+        @Override
+        public T next() {
+            if(hasNext()){
+                return array[nextElem++];
+            }
+            return null;
+        }
+    }
+}
---
+++ L03.2-MyArrayList/src/main/java/MyArrayListTest.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class MyArrayListTest {
+    public static void main(String[] args) {
+
+        Object[] string = {"xfg", "jfg", "abc", "abj"};
+        Arrays.sort(string);
+        System.out.println(Arrays.toString(string));
+
+        MyArrayList<Integer> integerMyArrayList = new MyArrayList<>(8);
+
+        for (int i = 0; i < 6; i++) {
+            integerMyArrayList.add(i);
+            System.out.println(integerMyArrayList.size());
+        }
+
+        List<String> stringArrayList = new MyArrayList<>(25);
+        stringArrayList.add("zero");
+        stringArrayList.add("two");
+        stringArrayList.add("three");
+        stringArrayList.add(1, "oneIns");
+
+        List<String> arrayList = new MyArrayList<>();
+        arrayList.add("otherOne");
+        arrayList.add("otherTwo");
+        arrayList.add("otherThree");
+
+        stringArrayList.addAll(arrayList);
+        stringArrayList.addAll(3, arrayList);
+
+        System.out.print(Arrays.toString(stringArrayList.toArray()));
+        System.out.println();
+
+        stringArrayList.remove(3);
+
+        System.out.print(Arrays.toString(stringArrayList.toArray()));
+        System.out.println();
+
+        MyArrayList.sort(stringArrayList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int result = o1.compareTo(o2);
+                return result;
+            }
+        });
+
+        System.out.print(Arrays.toString(stringArrayList.toArray()));
+        System.out.println();
+
+        MyArrayList.copy(stringArrayList, arrayList);
+
+        System.out.print(Arrays.toString(stringArrayList.toArray()));
+        System.out.println();
+    }
+
+}
